0ced1ec8d8d10750bdaf48f6ab58fb43b706b339
sarah-story <sarah.f.lavendol@vanderbilt.edu>
2015-10-23 14:31:41 -0500

    Natural Numbers implementation with tests

---
+++ MathmagicianTests/EvenTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using mathmagician;
+
+namespace MathmagicianTests
+{
+    [TestClass]
+    public class EvenTests
+    {
+        [TestMethod]
+        public void EnsureICanCreateClassInstance()
+        {
+            EvenNumbers even = new EvenNumbers();
+            Assert.IsNotNull(even);
+        }
+    }
+}
---
+++ mathmagician/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mathmagician
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string prompt = "> ";
+            Console.WriteLine("What do you want me to do?");
+            Console.Write(prompt);
+            string input = Console.ReadLine();
+            if (input.ToLower().Equals("natural numbers"))
+            {
+                Console.WriteLine("How many?");
+                Console.Write(prompt);
+                string response_length = Console.ReadLine();
+                int length;
+                bool parsed = int.TryParse(response_length, out length);
+                if (parsed)
+                {
+                    NaturalNumbers nats = new NaturalNumbers();
+                    Console.WriteLine(nats.ToString(nats.GetSequence(length)));
+                } else
+                {
+                    Console.WriteLine("Whoops!");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Nope! Do better next time.");
+            }
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey(); 
+        }
+    }
+}
